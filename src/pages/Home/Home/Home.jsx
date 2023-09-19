2c910193ba421d656dd784bfdefc11e05f533706
NayanSr <nayankbps@gmail.com>
2023-09-19 09:42:59 +0600

    sixtyNine-five

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,11 @@
+import About from "../About/About";
 import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
     <div className="bg-slate-100">
       <Banner />
-      <h2>This is home</h2>
+      <About />
     </div>
   );
 };

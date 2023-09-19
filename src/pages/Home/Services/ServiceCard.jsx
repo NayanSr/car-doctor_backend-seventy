60e3051e0106ce39e7d6eddeb3376faabd6ec89b
NayanSr <nayankbps@gmail.com>
2023-09-19 10:34:42 +0600

    sixtyNine-six

---
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -0,0 +1,23 @@
+// import React from "react";
+
+// eslint-disable-next-line react/prop-types
+const ServiceCard = ({ service }) => {
+  // eslint-disable-next-line react/prop-types
+  const { title, img, price } = service;
+  return (
+    <div className="card w-50 m-1 bg-base-100 shadow-xl border-red-600 border-solid border-2">
+      <figure className="px-2 pt-4">
+        <img src={img} alt={title} className="rounded-xl w-[230px] h-[175px]" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{title}</h2>
+        <p className="text-xl text-yellow-600">Price: ${price}</p>
+        <div className="card-actions">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCard;

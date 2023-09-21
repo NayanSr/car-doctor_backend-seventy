76a16bb9cc5ec3179431feea689e608e6dabbbf8
NayanSr <nayankbps@gmail.com>
2023-09-22 00:53:32 +0600

    'seventy_ui_full'

--- src/pages/Booking/BookingRow.jsx
+++ src/pages/Booking/BookingRow.jsx
@@ -1,10 +1,29 @@
-const BookingRow = ({ booking }) => {
-  const { service, customerName, email, price, img } = booking;
+const BookingRow = ({ booking, handleDelete, handleConfirm }) => {
+  const { _id, service, customerName, email, price, img, status } = booking;
+
   return (
     <tr>
       <th>
         <label>
-          <input type="checkbox" className="checkbox" />
+          <button
+            onClick={() => handleDelete(_id)}
+            className="btn btn-square btn-sm"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
         </label>
       </th>
       <td>
@@ -23,7 +42,16 @@ const BookingRow = ({ booking }) => {
       <td>{email}</td>
       <td>{price}</td>
       <th>
-        <button className="btn btn-ghost btn-xs">details</button>
+        {status === "confirm" ? (
+          <span className="font-bold text-green-700">Confirmed</span>
+        ) : (
+          <button
+            onClick={() => handleConfirm(_id)}
+            className="btn btn-ghost btn-xs text-orange-600"
+          >
+            Please Confirm
+          </button>
+        )}
       </th>
     </tr>
   );
